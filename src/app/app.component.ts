0dafab1767687289b847a579edc65d47ff60876a
JereVeijalainen <jereveijalainen82@gmail.com>
2019-03-07 14:33:13 +0200

    First commit of the game
    This is the first working version of the Rock Paper Scissors Game.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	title = 'Welcome to Rock Paper Scissors Game';
+	gameStarted = false;
+	playerItem: string;
+	opponentItem: string;
+	resultText: string;
+
+
+	startGame() {
+		this.gameStarted = true;
+	}
+
+	endGame() {
+		this.gameStarted = false;
+	}
+
+	chooseItem(item: string) {
+		const opponentItem = this.chooseOpponentItem();		
+		this.resultText = `You chose ${item}. Your opponent chose ${opponentItem}. Result: ${this.getResult(item, opponentItem)}.`;	
+	}
+
+	chooseOpponentItem(): string {
+		const items = ['rock', 'paper', 'scissors'];
+		return this.randomItem(items);
+	}
+
+	randomItem(items) {		
+		return items[Math.floor(Math.random() * items.length)];		
+	}
+
+	getResult(playerItem, opponentItem) {
+		var win = false;
+
+		if (playerItem === opponentItem) {
+			return 'Tie';
+		}
+		else {
+			switch (playerItem) {
+				case 'rock':
+					opponentItem === "scissors" ? win = true : win = false;
+				case 'paper':
+					opponentItem === "rock" ? win = true : win = false;
+				case 'scissors':
+					opponentItem === "paper" ? win = true : win = false;
+			}
+			return win ? 'You win' : 'You lose';
+		}
+
+	}
 }
